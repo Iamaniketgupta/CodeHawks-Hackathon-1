e7e706d19d8d03dab5d530564e1c39c84ca4a1b2
Aniket Gupta <aniketgupta.org@gmail.com>
2024-03-09 16:55:23 +0530

    added

--- frontend/src/components/Dashboard/MapSection.jsx
+++ frontend/src/components/Dashboard/MapSection.jsx
@@ -1,44 +1,11 @@
-import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from "react-leaflet";
-import AutoSearchBox from "./subSections/AutoSearchBox";
-import { useState } from "react";
+import NearbyUsersMap from './subSections/NearByUsersMap';
+
 const MapSection = () => {
 
-    function LocationMarker() {
-        const [position, setPosition] = useState(null)
-        const map = useMapEvents({
-          click() {
-            map.locate()
-          },
-          locationfound(e) {
-            setPosition(e.latlng)
-            map.flyTo(e.latlng, map.getZoom())
-          },
-        })
-      
-        return position === null ? null : (
-          <Marker position={position}>
-            <Popup>You are here</Popup>
-          </Marker>
-        )
-      }
-      
+    // filter using distance and intrests
     return (
         <div>
-            <AutoSearchBox />
-
-            <div>
-                <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-                    <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    <Marker position={[51.505, -0.09]}>
-                        <Popup>
-                            A pretty CSS3 popup. <br /> Easily customizable.
-                        </Popup>
-                    </Marker>
-                </MapContainer>
-            </div>
+            <NearbyUsersMap/>
         </div>
     );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,34 +1,39 @@
 import { MdDarkMode } from "react-icons/md";
 import { CiLight } from "react-icons/ci";
 import { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-const Header = ({dark,setDark}) => {
-    
+const Header = ({ dark, setDark }) => {
+    const location = useLocation();
 
     useEffect(() => {
         localStorage.setItem('darkMode', JSON.stringify(dark));
         document.documentElement.classList.toggle('dark', dark);
     }, [dark]);
- 
 
     return (
-        <div className='sticky top-0 flex h-14 dark:bg-[#0e1012]  bg-white text-slate-800 items-center justify-between px-8 py-3'>
+        <div className='sticky top-0 z-50 flex h-14 dark:bg-[#0e1012] bg-white text-slate-800 items-center justify-between px-8 py-3'>
             <div className="font-bold text-xl min-w-fit">
                 <Link to={"/"} className="dark:text-white text-slate-800 hover:text-inherit">
-                Code Hawks
+                    Code Hawks
                 </Link>
             </div>
-            <nav className=" max-w-fit flex items-center justify-center gap-2 pl-4">
+            <nav className="max-w-fit flex items-center justify-center gap-2 pl-4">
                 <div>
                     {dark ? (
-                        <CiLight size={25} className="cursor-pointer  text-yellow-300 shadow-lg font-bold" onClick={() => setDark(false)} title="Light Mode"/>
+                        <CiLight size={25} className="cursor-pointer text-yellow-300 shadow-lg font-bold" onClick={() => setDark(false)} title="Light Mode" />
                     ) : (
-                        <MdDarkMode size={25} className="cursor-pointer  hover:text-blue-700 dark:text-white text-slate-800" onClick={() => setDark(true)} title="Dark Mode"/>
+                        <MdDarkMode size={25} className="cursor-pointer hover:text-blue-700 dark:text-white text-slate-800" onClick={() => setDark(true)} title="Dark Mode" />
                     )}
                 </div>
-                <Link to={"/api/login"} className=' min-w-fit p-2 px-4  hover:text-blue-500 cursor-pointer font-semibold text-sm rounded-3xl' >Log in</Link>
-                <Link to={"/api/signup"} className='min-w-fit p-2 px-4  bg-blue-700 cursor-pointer hover:text-inherit text-white font-semibold text-sm rounded-3xl'>Sign up</Link>
+                {location.pathname !== "/api/dashboard" ? (
+                    <>
+                        <Link to={"/api/login"} className='min-w-fit p-2 px-4 hover:text-blue-500 cursor-pointer font-semibold text-sm rounded-3xl'>Log in</Link>
+                        <Link to={"/api/signup"} className='min-w-fit p-2 px-4 bg-blue-700 cursor-pointer hover:text-inherit text-white font-semibold text-sm rounded-3xl'>Sign up</Link>
+                    </>
+                ) : (
+                    <Link to={"/api/login"} className='min-w-fit p-2 px-4 hover:text-blue-500 cursor-pointer font-semibold text-sm rounded-3xl'>Log Out</Link>
+                )}
             </nav>
         </div>
     );
