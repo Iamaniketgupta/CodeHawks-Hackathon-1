2d45c4d633d3816a0455fe8ee290b8fcf6b70663
surajgsn07 <surajgn07@gmail.com>
2024-03-09 17:01:30 +0530

    added redux toolkit and logout feature and minor things

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,35 +1,61 @@
 import InputComp from "./subcomponents/InputComp";
 import Buttons from "./subcomponents/Buttons";
 import { useState } from "react";
+import { useNavigate} from 'react-router-dom'
+import { login, logout} from '../store/authSlice'
+import { useDispatch, useSelector } from 'react-redux';
 
+import {request} from '../constants.js'
+import {loginUser} from '../utils/user.data.fetch.js'
 const Login = () => {
     const [data, setData] = useState({
         email: "",
-        phone: "",
+        phoneNo: "",
         password: ""
+
     });
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const user = useSelector((state)=>state.auth.user)
+    
+    console.log(user)
+
+    const [giveEmail, setgiveEmail] = useState(true);
+    const changeParameter = ()=>{
+        setgiveEmail((prev)=>!prev);
+    }
 
     const loginHandler = async (e) => {
         e.preventDefault();
-    
-        try {
-            const response = await fetch('/api/v1/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            });
-    
-            if (response.ok) {
-                const responseData = await response.json();
-                console.log(responseData); 
-            } else {
-                console.error('Failed to log in:', response.statusText);
+
+        const response = await loginUser(data);
+        if(response){
+            const obj = {
+                user:response.data
             }
-        } catch (error) {
-            console.error('Error:', error);
+            dispatch(login(obj));
+            console.log(user)
+            navigate('/api/dashboard');
         }
+    
+        // try {
+        //     const response = await fetch(`${request}/user/login`, {
+        //         method: 'POST',
+        //         headers: {
+        //             'Content-Type': 'application/json'
+        //         },
+        //         body: JSON.stringify(data)
+        //     });
+    
+        //     if (response.ok) {
+        //         const responseData = await response.json();
+        //         console.log(responseData); 
+        //     } else {
+        //         console.error('Failed to log in:', response.statusText);
+        //     }
+        // } catch (error) {
+        //     console.error('Error:', error);
+        // }
     }
     
 
@@ -46,7 +72,8 @@ const Login = () => {
             <div className="flex flex-col justify-center w-[290px] h-fit py-8 px-5 text-white dark:text-slate-800 dark:bg-white bg-slate-800 text-inherit rounded-2xl">
                 <h2 className="font-bold text-2xl text-center my-3">Log In</h2>
                 <form onSubmit={loginHandler}>
-                    <InputComp
+                    {giveEmail ? (
+                        <InputComp
                         value={data.email}
                         name="email"
                         onChange={(e)=>handleSetData(e)}
@@ -56,16 +83,21 @@ const Login = () => {
                         required
                         placeholder="Enter Email"
                     />
-                    <p className="text-center font-semibold">OR</p>
-                    <InputComp
+                    ) : (
+                        <InputComp
                         value={data.phone}
                         onChange={(e)=>handleSetData(e)}
                         type="tel"
                         label="Phone"
-                        id="phone"
-                        name="phone"
+                        id="phoneNo"
+                        name="phoneNo"
                         placeholder="Enter Phone"
                     />
+                    )}
+                    <button onClick={changeParameter} className="dark:text-white">
+                        {giveEmail ? "Use Phone Number":"Use email"}
+                    </button>
+                    
                     <InputComp
                         value={data.password}
                         onChange={(e)=>handleSetData(e)}
